d60334ec228ce7b0848a4b9af98d7d6ad6d124c6
zipypie <201980170@psu.palawan.edu.ph>
2023-06-01 20:05:45 +0800

    added xml format and jsonify as default

--- mainAPI.py
+++ mainAPI.py
@@ -1,13 +1,14 @@
 from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
+import xml.etree.ElementTree as ET
+
 app = Flask(__name__)
 
-app.config["MYSQL_HOST"]= "localhost"
-app.config["MYSQL_USER"]= "root"
-app.config["MYSQL_PASS"]= ""
-app.config["MYSQL_DB"]="mobile_purchases_db"
-
-app.config["MYSQL_CURSOSCLASS"]= "DictCursor"
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASS"] = ""
+app.config["MYSQL_DB"] = "mobile_purchases_db"
+app.config["MYSQL_CURSOSCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 
@@ -15,16 +16,37 @@ mysql = MySQL(app)
 def hello_world():
     return "<p>Hello World</p>"
 
-@app.route("/customers",methods=["GET"])
+@app.route("/customers", methods=["GET"])
 def get_customers():
-  cur = mysql.connection.cursor()
-  query="""
-  select * from customer
-  """
-  cur.execute(query)
-  data = cur.fetchall()
-  cur.close()
-  return make_response(jsonify(data),200)
+    format = request.args.get("format", "").lower()
+    cur = mysql.connection.cursor()
+    query = """
+    SELECT * FROM customer
+    """
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+
+    if format == "xml":
+        # Convert data to XML format
+        root = ET.Element("customers")
+        for row in data:
+            customer = ET.SubElement(root, "customer")
+            for key, value in row.items():
+                field = ET.SubElement(customer, key)
+                field.text = str(value)
+
+        # Convert XML to string
+        xml_data = ET.tostring(root, encoding="utf-8")
+
+        # Set the Content-Type header to indicate XML format
+        headers = {"Content-Type": "application/xml"}
+
+        # Return the XML response
+        return make_response(xml_data, 200, headers)
+    else:
+        # Return the JSON response
+        return make_response(jsonify(data), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
