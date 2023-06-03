9d7f0b47c4b94e74e4332f46cd4cf87318aaae06
zipypie <201980170@psu.palawan.edu.ph>
2023-06-03 09:29:47 +0800

    added the unittest.py

---
+++ unittest.py
@@ -0,0 +1,23 @@
+import unittest
+import warnings
+from api import app
+
+class MyAppTests(unittest.TestCase):
+    def setUp(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+        
+        warnings.simplefilter("ignore",category=DeprecationWarning)
+        
+def test_index_page(self):
+    response = self.app.get("/")
+    self.assertEqual(response.status_code, 200)
+    self.assertEqual(response.data.decode(),"<p>Hello, World!</p>")
+    
+    def test_getcustomers(self):
+        response = self.app.get("/customers")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual("Santos" in response.data.decode())
+        
+    if __name__ == "__main__":
+        unittest.main()
