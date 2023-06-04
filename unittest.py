f59013c7b78ae59df4bfc4af3dfc1c24a09742d8
zipypie <201980170@psu.palawan.edu.ph>
2023-06-04 18:37:25 +0800

    modified the def method

--- unittest.py
+++ unittest.py
@@ -6,18 +6,18 @@ class MyAppTests(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         self.app = app.test_client()
-        
-        warnings.simplefilter("ignore",category=DeprecationWarning)
-        
-def test_index_page(self):
-    response = self.app.get("/")
-    self.assertEqual(response.status_code, 200)
-    self.assertEqual(response.data.decode(),"<p>Hello, World!</p>")
-    
-    def test_getcustomers(self):
+
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+    def test_index_page(self):
+        response = self.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data.decode(), "<p>Hello, World!</p>")
+
+    def test_get_customers(self):
         response = self.app.get("/customers")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual("Santos" in response.data.decode())
-        
-    if __name__ == "__main__":
-        unittest.main()
+        self.assertEqual("Santos" in response.data.decode(), True)
+
+if __name__ == "__main__":
+    unittest.main()
